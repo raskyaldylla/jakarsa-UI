5e09a2e619abbba5446f810e06621e19df98f9fa
raskyaldylla <raskyaldylla@gmail.com>
2020-12-21 01:47:17 +0700

    service section updated

--- src/Sections/Services.svelte
+++ src/Sections/Services.svelte
@@ -1,29 +1,34 @@
 
 <div class="p-8 h-full w-full text-white flex justify-center flex-col">
   <h2 class="md:text-4xl text-3xl font-title font-black leading-tight">Our Services</h2>
+  <p class="md:w-1/2 text-xs md:text-sm">We are able to meet the needs of consumers on the field of delivery of agricultural focus exports by upholding professionalism reliable, competitive and friendly service.</p>
   <div class="flex flex-no-wrap min-w-full md:w-full overflow-x-auto md:overflow-x-hidden py-12 no-scrollbar">
-    <div class="w-64 md:w-1/4 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
-      <h3 class="font-black text-xl leading-loose">Service Alpha</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa distinctio sapiente eveniet exercitationem molestiae officiis, cupiditate quam pariatur at eligendi suscipit saepe laudantium iure sit enim asperiores tempore aut voluptas?</p>
+    <div class="w-64 md:w-1/5 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
+      <h3 class="font-black text-xl leading-loose">Exclusive Products</h3>
+      <p>We provides CPO and PKO with the best quality over in Indonesia referring to RSPO, ISPO, ISCC, UNGC, Codex Stan 210-1999. Become our exclusive client and you alone will have the right to sell one or many of our products in your market.</p>
     </div>
-    <div class="w-64 md:w-1/4 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
-      <h3 class="font-black text-xl leading-loose">Service Beta</h3>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis exercitationem nesciunt a harum atque. At, magni est vitae similique cupiditate quo, id, unde dolore blanditiis iure reiciendis rem repellat illum.</p>
+    <div class="w-64 md:w-1/5 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
+      <h3 class="font-black text-xl leading-loose">Quality Assurance</h3>
+      <p>We care, we share. Our procedure of Quality Control is aim to meet your expectations. We took 3 steps of Quality Control to determining the qualities of the first step from the raw materials, production process until the finished goods.</p>
     </div>
-    <div class="w-64 md:w-1/4 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
-      <h3 class="font-black text-xl leading-loose">Service Gamma</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui fugit nostrum totam tempora, dolorem fugiat? Blanditiis, expedita aut rem porro officia atque suscipit, eligendi dolores velit repudiandae quam nesciunt aperiam.</p>
+    <div class="w-64 md:w-1/5 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
+      <h3 class="font-black text-xl leading-loose">Shipping</h3>
+      <p>Since our Company's inception, our motive is to meet our buyer's expectation. In terms of price and quality and from order to execution for each shipment which has grained us to grow tremendously, from customer satisfaction. We are always costumer focused and will deliver what the customer needs, in terms of value, Quality and Satisfaction.</p>
     </div>
-    <div class="w-64 md:w-1/4 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
-      <h3 class="font-black text-xl leading-loose">Service Epsilon</h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem odio reiciendis ut in illum culpa eius ipsum, quod nihil aspernatur aliquid ullam quam expedita voluptate! Delectus dignissimos natus sed quis.</p>
+    <div class="w-64 md:w-1/5 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
+      <h3 class="font-black text-xl leading-loose">Marketing Support</h3>
+      <p>When you become a distributor of one our products, we will advertise your company as our representative, and redirect all customers that show interest in our products to you.</p>
+    </div>
+    <div class="w-64 md:w-1/5 md:flex-shrink-1 flex-shrink-0 mx-4 md:mx-0 px-0 md:px-4">
+      <h3 class="font-black text-xl leading-none">Direct Connection with the Factory</h3>
+      <p>We work directly as part of the factory to ensure our costumers the highest profit margin. This also means that working with us means working with the factory itself.</p>
     </div>
   </div>
 </div>
 
 <style>
   .no-scrollbar::-webkit-scrollbar {
-    @apply h-1;
+    @apply h-1 w-1;
   }
   .no-scrollbar::-webkit-scrollbar-track {
     @apply h-1 bg-transparent;
