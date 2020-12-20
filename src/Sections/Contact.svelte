bad66085099b52f591e4333f3fe03668a20ed550
raskyaldylla <raskyaldylla@gmail.com>
2020-12-21 01:24:54 +0700

    contact page updated

--- src/Sections/Contact.svelte
+++ src/Sections/Contact.svelte
@@ -8,7 +8,7 @@ import Icons from "../Components/Icons.svelte";
 <div class="p-8 h-full w-full text-white">
   <div class="flex h-full w-full flex-no-wrap flex-col md:flex-row items-center justify-center md:justify-between">
     <div class="w-full md:mr-6" style="height: 50%">
-      <iframe title="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.4751479756092!2d106.747831542304!3d-6.276799113008376!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f041b2fa0fe7%3A0xe924d7063b42f260!2sTaman%20Kepodang!5e0!3m2!1sid!2sid!4v1606755048284!5m2!1sid!2sid" width="100%" height="100%" frameborder="0" style="border:0;" aria-hidden="false" tabindex="0"></iframe>
+      <iframe title="maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.317392338844!2d106.7804625145985!3d-6.221813388007533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f12d82b5cad9%3A0xec3043c856077a11!2sThe%20Bellezza%20Shopping%20Arcade!5e0!3m2!1sid!2sid!4v1608488602602!5m2!1sid!2sid" width="100%" height="100%" frameborder="0" style="border:0;" aria-hidden="false" tabindex="0"></iframe>
     </div>
     <div class="w-full mt-6 md:mt-0">
       <h2 class="md:text-4xl text-3xl font-title font-black leading-tight mb-8">Reach Us</h2>
@@ -20,13 +20,13 @@ import Icons from "../Components/Icons.svelte";
       </p>
       <div class="flex flex-no-wrap mt-4">
         <div>
-          <figure class="w-12 p-3 mr-2 rounded-full border-2 border-transparent hover:border-white hoverable"><Icons name="email"></Icons></figure>
+          <figure class="w-12 p-3 mr-2 rounded-full border-2 border-transparent hover:border-white hoverable" on:click={()=> {window.open('mailto:jakarsa.m.internasional@gmail.com', "_blank")}}><Icons name="email"></Icons></figure>
         </div>
         <div>
           <figure class="w-12 p-3 mr-2 rounded-full border-2 border-transparent hover:border-white hoverable"><Icons name="instagram"></Icons></figure>
         </div>
         <div>
-          <figure class="w-12 p-3 mr-2 rounded-full border-2 border-transparent hover:border-white hoverable"><Icons name="whatsapp"></Icons></figure>
+          <figure class="w-12 p-3 mr-2 rounded-full border-2 border-transparent hover:border-white hoverable" on:click={()=> {window.open('https://wa.me/6281286020289', "_blank")}}><Icons name="whatsapp"></Icons></figure>
         </div>
       </div>
     </div>
