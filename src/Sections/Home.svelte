79e89985b2ee9065c064e94d68746407d87cb9f3
raskyaldylla <raskyaldylla@gmail.com>
2020-12-21 00:40:59 +0700

    force text to be centered

--- src/Sections/Home.svelte
+++ src/Sections/Home.svelte
@@ -1,5 +1,5 @@
 
 <div class="p-8 h-full w-full md:w-3/4 mx-auto flex flex-no-wrap flex-col justify-center items-center text-left select-none">
-  <p>Now, you reach the right website!</p>
-  <h2 class="md:text-4xl text-3xl font-title font-black leading-tight">We’are Specialized in <span class="text-primary-light">Agriculture Products</span></h2>
+  <p class="text-center">Now, you reach the right website!</p>
+  <h2 class="md:text-4xl text-3xl font-title font-black leading-tight text-center">We’are Specialized in <span class="text-primary-light">Agriculture Products</span></h2>
 </div>
