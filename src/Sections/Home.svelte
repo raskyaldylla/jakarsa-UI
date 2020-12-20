159e997e1c319b58717de2a9988cf56b5ac83bf8
raskyaldylla <raskyaldylla@gmail.com>
2020-12-21 00:14:15 +0700

    home page content update

--- src/Sections/Home.svelte
+++ src/Sections/Home.svelte
@@ -1,5 +1,5 @@
 
 <div class="p-8 h-full w-full md:w-3/4 mx-auto flex flex-no-wrap flex-col justify-center items-center text-left select-none">
-  <h2 class="md:text-4xl text-3xl font-title font-black leading-tight">We are the new kids on the block. A <b class="text-primary-light">Disruptor</b>, a new challenger, your friendly partner.</h2>
-  <p>Praesent commodo mi eu efficitur ultricies. Nam congue consectetur augue. Maecenas rhoncus scelerisque leo, vitae efficitur nunc iaculis non. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+  <p>Now, you reach the right website!</p>
+  <h2 class="md:text-4xl text-3xl font-title font-black leading-tight">We’are Specialized in <span class="text-primary-light">Agriculture Products</span></h2>
 </div>
