cddb138b8f9a1a37e5a4ec4c1b62ceaba446d3b6
raskyaldylla <raskyaldylla@gmail.com>
2020-12-21 01:58:35 +0700

    set commodities section blank

--- src/Sections/Commodities.svelte
+++ src/Sections/Commodities.svelte
@@ -7,7 +7,5 @@
 
 <div class="h-full w-full overflow-auto relative">
   <article class="w-full h-full absolute left-0 top-0 p-8" transition:fade={{duration:350}}>
-    <p class="text-center">Now, you reach the right website!</p>
-    <h2 class="md:text-4xl text-3xl font-title font-black leading-tight text-center">We’are Specialized in <span class="text-primary-light">Agriculture Products</span></h2>
   </article>
 </div>
